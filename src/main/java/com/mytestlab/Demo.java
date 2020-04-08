2ff8ef4b3a1af8fe5c877035a0b8197d38db92ec
vivek kumar <vivekhaarit@gmail.com>
2020-04-08 16:25:43 +0530

    generic constructor made

--- src/main/java/com/mytestlab/Demo.java
+++ src/main/java/com/mytestlab/Demo.java
@@ -2,7 +2,12 @@ package com.mytestlab;
 
 import java.io.Serializable;
 
-class Demo {
+class Demo <T> {
+    T[] myArray;
+    public Demo(T[] myArray){
+        this.myArray=myArray;
+    }
+
     public static void main(String[] args) {
         Integer integerArray[]={1,2,3};
         Double doubleArray[]={1.1,2.2,3.3};
@@ -11,8 +16,15 @@ class Demo {
         printIntegerArray(integerArray);
         printIntegerArray(charArray);
         printIntegerArray(doubleArray);
-    }
 
+        new Demo<Integer>(integerArray).toPrint();
+        new Demo<Double>(doubleArray).toPrint();
+        new Demo<Character>(charArray).toPrint();
+
+    }
+    private void toPrint(){
+        printIntegerArray(myArray);
+    }
     private static <T> void printIntegerArray(T[] integerArray){
         for (T num:integerArray) {
             System.out.println("Integer no is: "+num);
